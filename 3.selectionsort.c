9f086be8870fec4d0d9cd549e258252916770af6
Virtuoso-8051 <rajanurag1508@gmail.com>
2024-09-28 15:40:08 +0530

    Add files via upload

---
+++ 3.selectionsort.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <limits.h>
+int main(){
+    int n;
+    printf("enter the size of array:");
+    scanf("%d",&n);
+    
+    int arr[n];
+    printf("enter the elements of array:");
+    for(int i=0;i<n;i++){
+        scanf("%d",&arr[i]);
+    }
+
+    for(int i=0;i<n-1;i++){
+        int min=INT_MAX;
+        int j;
+        for(j=i;j<n;j++){
+            if(arr[j]<min){
+                min=arr[j];
+            }
+        }
+            int temp=min;
+            min=arr[i];
+            arr[i]=temp;
+    }
+    printf("sorted array is:");
+    for(int i=0;i<n;i++){
+        printf("%d ",arr[i]);
+    }
+    return 0;
+}
